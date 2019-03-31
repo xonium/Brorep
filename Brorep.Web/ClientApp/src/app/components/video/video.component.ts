fac8d369ea381ece5a261672487909673eb75425
Unknown <johan.erik.obrink@gmail.com>
2019-03-31 11:19:17 +0200

    adding more video capabilites

--- Brorep.Web/ClientApp/src/app/components/video/video.component.ts
+++ Brorep.Web/ClientApp/src/app/components/video/video.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, SimpleChanges, SimpleChange } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, SimpleChange, EventEmitter, Output, OnChanges } from '@angular/core';
 import { RenderReactPlayer } from './reactplayer.standalone';
 @Component({
   selector: 'app-video',
@@ -7,10 +7,14 @@ import { RenderReactPlayer } from './reactplayer.standalone';
       </div>
     </div>`
 })
-export class VideoComponent implements OnInit {
+export class VideoComponent implements OnInit, OnChanges  {
   constructor() { }
   @Input() videoUrl: string;
   private _videoUrl: string;
+
+  @Output() errorEvent = new EventEmitter();
+  @Output() progressEvent = new EventEmitter();
+  @Output() readyEvent = new EventEmitter();
 
   ngOnInit() {
         if (this._videoUrl) {
@@ -20,22 +24,39 @@ export class VideoComponent implements OnInit {
         }
     }
 
-    ngOnChanges(changes: SimpleChanges) {
+    ngOnChanges(changes: SimpleChanges): void {
         const inputvideoUrl: SimpleChange = changes.videoUrl;
+        const that = this;
         this._videoUrl = inputvideoUrl.currentValue;
         if (this._videoUrl) {
             const container = document.getElementById('player');
             const url = this.videoUrl;
-            RenderReactPlayer(container, { url, playing: true, onPlay: this.onPlay, onProgress: this.onProgress });
+            RenderReactPlayer(container,
+                {
+                    url,
+                    playing: true,
+                    onPlay: onVideoPlay,
+                    onProgress: onVideoProgress,
+                    onError: onVideoError,
+                    onReady: onVideoReady
+                });
         }
-    }
 
-    onPlay() {
-        console.log('playing!');
-    }
+        function onVideoProgress(progress) {
+            that.progressEvent.emit(progress);
+        }
 
-    onProgress(progress) {
-        console.log('progress', progress);
+        function onVideoPlay() {
+            console.log('playing!');
+        }
+
+        function onVideoError(error) {
+            that.errorEvent.emit(error);
+        }
+
+        function onVideoReady() {
+            that.readyEvent.emit();
+        }
     }
   }
 
--- Brorep.Web/ClientApp/src/app/components/workout/workout.component.ts
+++ Brorep.Web/ClientApp/src/app/components/workout/workout.component.ts
@@ -10,6 +10,8 @@ export class WorkoutComponent implements OnInit {
 
     loadThisVideo: string;
     loadVideoForm: FormGroup;
+    seekerLocation: number;
+    videoReady: boolean;
 
     constructor(private fb: FormBuilder) {
         this.loadVideoForm = this.fb.group({
@@ -21,5 +23,22 @@ export class WorkoutComponent implements OnInit {
 
     onLoadVideoFormSubmit() {
         this.loadThisVideo = this.loadVideoForm.get('videoUrl').value;
+    }
+
+    onVideoReady(event) {
+        this.videoReady = true;
+    }
+
+    onVideoError(error) {
+        console.log(error);
+    }
+
+    onSetSeekerLocation(value) {
+        console.log('SEEKERLOCATION', value);
+    }
+
+    onVideoProgress(progress) {
+        console.log(progress);
+        this.seekerLocation = progress.played;
     }
 }
